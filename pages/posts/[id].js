29d350d8a21c9f0b8a36a40f27da6bc2da744947
chen <chen@kt-elektronik.de>
2022-11-02 17:21:49 +0100

    init 2

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,30 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from '../../styles/utils.module.css';
 
-export default function Post({postData}) {
+export default function MyPost({postData}) {
     return (
         <Layout>
-            {postData.title}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+
+            {/*{postData.title}
             <br />
             {postData.id}
             <br />
-            {postData.date}
+            <Date dateString={postData.date} />
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />*/}
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>)
 }
 
@@ -21,7 +37,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id);
+    const postData = await getPostData(params.id);
     return {
         props: {
             postData,
